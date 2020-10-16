13d1f0ee9d0b7da38c23fa5bca16fae7a41e4b2c
Emily Hartzell <emily.g.hartzell@gmail.com>
2020-10-16 13:57:07 -0500

    can fire an employee

--- src/components/employee/EmployeeProvider.js
+++ src/components/employee/EmployeeProvider.js
@@ -29,6 +29,13 @@ export const EmployeeProvider = (props) => {
             .then(getEmployees)
     }
 
+    const fireEmployee = (employeeId) => {
+        return fetch(`http://localhost:8088/employees/${employeeId}`, {
+            method: "DELETE"
+        })
+            .then(getEmployees)
+    }
+
     /*
         You return a context provider which has the
         `Employees` state, the `addEmployee` function,
@@ -37,7 +44,7 @@ export const EmployeeProvider = (props) => {
     */
     return (
         <EmployeeContext.Provider value={{
-            employees, addEmployee, getEmployees
+            employees, addEmployee, getEmployees, fireEmployee
         }}>
             {props.children}
         </EmployeeContext.Provider>
